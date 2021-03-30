ca56972854661b85d1a45da8584e709f94631bd6
Tim Holmes <tpholmes7@gmail.com>
2021-03-30 16:22:15 -0500

    Added classes for spring 2021

---
+++ phy-474/scripts/join_class.sh
@@ -0,0 +1,11 @@
+echo -----------------
+echo -Joining PHY 474-
+echo -----------------
+
+#open "https://depaul.zoom.us/j/91802263914"
+open "https://d2l.depaul.edu/d2l/home/811863"
+open "zoommtg://zoom.us/join?confno=99823831860"
+
+echo ----------------
+echo -Joined PHY 474-
+echo ----------------
